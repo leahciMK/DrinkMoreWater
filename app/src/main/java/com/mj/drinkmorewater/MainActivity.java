c665c5b4088ec9e4a4ea33de6d630913c9966766
Miha <igor.kloar@gmail.com>
2017-12-06 17:09:01 +0100

    starting project

---
+++ app/src/main/java/com/mj/drinkmorewater/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mj.drinkmorewater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
