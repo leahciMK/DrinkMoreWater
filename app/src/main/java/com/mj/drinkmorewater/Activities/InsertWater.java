3b505a09f3b90736c41b014d4a3df76d936bdb63
julijanjug <julijan.jug@gmail.com>
2017-12-18 22:15:41 +0100

    improvments on settings activity

--- app/src/main/java/com/mj/drinkmorewater/Activities/InsertWater.java
+++ app/src/main/java/com/mj/drinkmorewater/Activities/InsertWater.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
--- app/src/main/java/com/mj/drinkmorewater/Activities/SettingsActivity.java
+++ app/src/main/java/com/mj/drinkmorewater/Activities/SettingsActivity.java
@@ -3,23 +3,27 @@ package com.mj.drinkmorewater.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.mj.drinkmorewater.R;
 import com.mj.drinkmorewater.db.DatabaseHandler;
 import com.mj.drinkmorewater.db.Water;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     SeekBar seekBarAge;
     SeekBar seekBarWeight;
     Spinner spinnerGender;
     Button  btnSave;
     CheckBox checkbox;
+    TextView ageText;
+    TextView weightText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,49 @@ public class SettingsActivity extends AppCompatActivity {
         spinnerGender = (Spinner)findViewById(R.id.gender_spinner);
         btnSave  = (Button)findViewById(R.id.save_btn);
         checkbox = (CheckBox)findViewById(R.id.checkBox);
+        ageText = (TextView)findViewById(R.id.age_textView);
+        weightText = (TextView)findViewById(R.id.weight_textView);
 
+        seekBarAge.setOnSeekBarChangeListener(this);
+        seekBarWeight.setOnSeekBarChangeListener(this);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar2);
+
+        setSupportActionBar(myToolbar);
+        myToolbar.setNavigationIcon(R.drawable.ic_action_back);
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        switch (seekBar.getId()) {
+            case R.id.age_seekBar:
+                ageText.setText(Integer.toString((int) seekBarAge.getProgress()) + " y");
+                break;
+
+            case R.id.weight_seekBar:
+                weightText.setText(Integer.toString((int) seekBarWeight.getProgress()) + " kg");
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBarAge) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBarAge) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
